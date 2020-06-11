1b72a428b0e5827c79fec92148863ba3148b6ac9
Luke Wu <daihere1993@gmail.com>
2020-06-12 00:06:32 +0800

    refactor - branch-selector

--- src/app/core/components/branch-selector/branch-selector.component.ts
+++ src/app/core/components/branch-selector/branch-selector.component.ts
@@ -17,18 +17,14 @@ import { IpcService } from '../../services/electron/ipc.service';
 export class BranchSelectorComponent {
   @Output() valueChange = new EventEmitter();
 
-  private _value: BranchInfo;
-  @Input()
-  public get value(): BranchInfo {
-    return this._value ? this._value : this.branches && this.branches[0];
-  }
-
-  public set value(value: BranchInfo) {
-    this.valueChange.emit(value);
-    this._value = value;
-  }
+  @Input() value: BranchInfo;
 
   @Input() branches: BranchInfo[] = [];
+
+  private setSelection(value: BranchInfo) {
+    this.value = value;
+    this.valueChange.emit(value);
+  }
 
   constructor(private dialogService: NbDialogService, private ipcService: IpcService) {}
 
@@ -37,7 +33,7 @@ export class BranchSelectorComponent {
       if (res && res.action === DialogAction.SAVE) {
         this.branches.push(res.content);
         setTimeout(() => {
-          this.value = res.content;
+          this.setSelection(res.content);
         }, 0);
         this.ipcService.send<{ key: string; value: BranchInfo[] }>(IPCMessage.STORE_DATA_REQ, {
           data: { key: 'branches', value: this.branches },
@@ -59,7 +55,7 @@ export class BranchSelectorComponent {
           const index = this.branches.findIndex((item) => item.name === branch.name);
           this.branches.splice(index, 1);
           if (this.value && this.value.name === res.content.name) {
-            [this.value] = this.branches;
+            this.setSelection(this.branches[0]);
           }
           this.ipcService.send<{ key: string; value: BranchInfo[] }>(IPCMessage.STORE_DATA_REQ, {
             data: { key: 'branches', value: this.branches },
